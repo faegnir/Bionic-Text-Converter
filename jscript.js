d4b4db3f88f817da178c4510f1557bc967991fe1
Emre Şen <emre.senn97@gmail.com>
2022-05-19 02:56:57 +0300

    css and js changes

---
+++ jscript.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    z = document.getElementById("myInput").value;
+   
+    var x=z.replace(/\b..../g, function(m){ return m.bold(); });
+    document.getElementById("aa").innerHTML = x;
+  }
+  function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Convert to Bionic";
+  }
+function copythis(){
+    var copyText = document.getElementById("aa");
+    
+    navigator.clipboard.writeText(copyText.innerHTML);
+
+}
+  
